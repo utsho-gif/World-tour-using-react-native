ec50621fda92ee2347d38072f269e7ddb67ad059
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-06-02 15:24:46 +0600

    added searched filter

--- component/Country.js
+++ component/Country.js
@@ -1,23 +1,47 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, StyleSheet, TextInput } from "react-native";
 import React, { useState, useEffect } from "react";
 import SingleCountry from "./SingleCountry";
 
 export default function Country() {
   const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
-      .then((data) => setCountries(data));
+      .then((data) => {
+        setSearch(data)
+        setCountries(data)
+      });
   }, []);
+
+  const handleSearch = text => {
+    const filtered = countries.filter(country => country.name.common.includes(text));
+    setSearch(filtered);
+  }
 
   return (
     <View>
-      <Text>Countries: {countries.length}</Text>
+      <Text style={styles.header}>Countries: {search.length}</Text>
+      <TextInput onChangeText={handleSearch} style={styles.input}></TextInput>
       <ScrollView>
-        {countries.map((country) => (
-          <SingleCountry key={country.id} country={country}></SingleCountry>
+        {search.map((country) => (
+          <SingleCountry key={country.ccn3} country={country}></SingleCountry>
         ))}
       </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 40,
+    fontSize: 40,
+    color: 'brown'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
+})
--- component/SingleCountry.js
+++ component/SingleCountry.js
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
 
-export default function SingleCountry({country}) {
-    const {name} = country;
+export default function SingleCountry({ country }) {
+  const { name, flags } = country;
   return (
     <View>
-      <Text>{name.common}</Text>
+      <Image
+        source={{
+          uri: flags.png,
+        }}
+        style={{ height: 200, width: 200, borderRadius: 100 }}
+      ></Image>
+      <Text style={{fontSize: 20, fontWeight: "bold", marginBottom: 20, marginTop: 20}}>{name.common}</Text>
     </View>
-  )
+  );
 }
